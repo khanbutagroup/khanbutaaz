c865f5a40b67b0d3480eee6461568068cd0f1087
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-03-08 10:57:23 +0400

    m

--- khanbuta/khanbuta/settings.py
+++ khanbuta/khanbuta/settings.py
@@ -68,6 +68,7 @@ PARLER_LANGUAGES = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',

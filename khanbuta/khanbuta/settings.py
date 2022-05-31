8bb1b5bc00e36e850fc2ba0767729bf3d277ad06
Fuad <quluzadef4@gmail.com>
2022-05-31 08:05:48 +0000

    serv

--- khanbuta/khanbuta/settings.py
+++ khanbuta/khanbuta/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ki&j@xcqsezp0l94*8*304*!u27r@0wu7xszms$9vdu=trj=&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
